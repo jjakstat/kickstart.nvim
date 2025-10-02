1cdc74b0993c580a0547de4584ee670a569bf72b
jjakstat <jannik@jakstat.de>
2025-10-02 17:50:14 +0200

    Added one dark theme

--- init.lua
+++ init.lua
@@ -937,6 +937,23 @@ require('lazy').setup({
   },
 
   -- [[ Colorscheme ]]
+  --{ -- You can easily change to a different colorscheme.
+  -- Change the name of the colorscheme plugin below, and then
+  -- change the command in the config to whatever the name of that colorscheme is.
+  --
+  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker',
+      }
+      -- Enable theme
+      require('onedark').load()
+      -- vim.cmd 'colorscheme github_dark_dimmed'
+    end,
+  },
   {
     'projekt0n/github-nvim-theme',
     name = 'github-theme',
@@ -946,31 +963,9 @@ require('lazy').setup({
       require('github-theme').setup {
         -- ...
       }
-
-      vim.cmd 'colorscheme github_dark_dimmed'
+      -- vim.cmd 'colorscheme github_dark_dimmed'
     end,
   },
-  --{ -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --'folke/tokyonight.nvim',
-  --priority = 1000, -- Make sure to load this before all the other start plugins.
-  --config = function()
-  --  ---@diagnostic disable-next-line: missing-fields
-  --  require('tokyonight').setup {
-  --    styles = {
-  --      comments = { italic = false }, -- Disable italics in comments
-  --    },
-  --  }
-
-  -- Load the colorscheme here.
-  -- Like many other themes, this one has different styles, and you could load
-  -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --  vim.cmd.colorscheme 'tokyonight-night'
-  --end,
-  --},
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
