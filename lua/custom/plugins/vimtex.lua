b6c132482cf23790d4d39421e0782a193396c8b3
jjakstat <jannik@jakstat.de>
2025-10-28 17:29:47 +0100

    Added umlaute

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -11,5 +11,6 @@ return {
     vim.g.tex_conceal = 'abdmg'
     -- Dont open warning window when compiling
     vim.g.vimtex_quickfix_open_on_warning = 0
+    vim.g.vimtex_quickfix_mode = 0
   end,
 }
