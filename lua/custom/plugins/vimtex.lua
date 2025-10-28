d36d403189ed845d45bf0e1aab9e88fbf14699e0
jjakstat <jannik@jakstat.de>
2025-10-28 14:29:34 +0100

    Added latex support and changed localleader to ,

---
+++ lua/custom/plugins/vimtex.lua
@@ -0,0 +1,13 @@
+return {
+  'lervag/vimtex',
+  lazy = false,
+  init = function()
+    vim.g.vimtex_view_method = 'skim'
+    vim.g.vimtex_view_skim_sync = 1
+    vim.g.vimtex_view_skim_activate = 1
+    vim.g.vimtex_compiler_method = 'latexmk'
+
+    vim.opt.conceallevel = 1
+    vim.g.tex_conceal = 'abdmg'
+  end,
+}
